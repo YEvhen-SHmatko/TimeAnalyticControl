78688cb47f1641f779f0f8c5a9fb4b5d0c6ddeae
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-04-22 09:07:57 +0300

    reinit, plush sass, remove TS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-
+import "typeface-roboto";
+import "./styles/styles.sass";
 ReactDOM.render(
   <BrowserRouter>
     <StrictMode>
